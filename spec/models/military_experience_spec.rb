b374eb095b1b15fb642720b91e359becb7027b26
ponlok huot <phuot1980@gmail.com>
2015-02-08 20:43:05 -0800

    redid test for military-experience

--- spec/models/military_experience_spec.rb
+++ spec/models/military_experience_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe MilitaryExperience, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+  	it "should have a valid factory" do 
+  		me = build(:military_experience)
+  		expect(me.save).to eq true
+  	end 
+		it { should belong_to(:user) }  	
+  	it { should validate_presence_of(:branch) } 
+  	it { should validate_presence_of(:mos) } 
+  	
+
+  end
+
 end
