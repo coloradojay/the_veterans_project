73bca3eedd03c55dccab4b8eca6369d364c611c5
ponlok huot <phuot1980@gmail.com>
2015-02-08 19:04:22 -0800

    renamed military experience test files

--- spec/factories/military_experiences.rb
+++ spec/factories/military_experiences.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :military_exp do
+  factory :military_experience do
     branch "MyString"
 mos "MyString"
 start_date "2015-02-08 16:37:51"
--- spec/models/military_experience_spec.rb
+++ spec/models/military_experience_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe MilitaryExp, type: :model do
+RSpec.describe MilitaryExperience, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
