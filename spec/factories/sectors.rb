8bd15c0be5db55a6e0bd5c493773c4e338cbd20c
ponlok huot <phuot1980@gmail.com>
2015-02-11 16:14:03 -0800

    fixed factory for types and sector

--- spec/factories/sectors.rb
+++ spec/factories/sectors.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :sector do
     name "Technical Services"
+    company
   end
 
 end
--- spec/factories/types.rb
+++ spec/factories/types.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :type do
     name "Public"
+    company
   end
 
 end
