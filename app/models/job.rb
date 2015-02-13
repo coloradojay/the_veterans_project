a4365d480f00985327e0e2282a00ef2527e16057
ponlok huot <phuot1980@gmail.com>
2015-02-12 18:35:08 -0800

    create job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,9 @@
 class Job < ActiveRecord::Base
+	has_one :company
+
+	validates :title, presence: true
+	validates :city, presence: true
+	validates :state, presence: true
+	validates :description, presence: true
+	validates :salary, presence: true
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do 
+  	it "should have a valid factory" do 
+  		j = build(:job)
+  		expect(j.save).to eq true
+  end
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  
 end
