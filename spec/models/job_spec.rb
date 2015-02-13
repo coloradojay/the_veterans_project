5b07f49f66e9608af33300b87559cdad207b5909
coloradojay <delarosa.j@gmail.com>
2015-02-12 18:40:46 -0800

    Resolving conflict

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,28 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-<<<<<<< HEAD
-	describe "validations" do 
-	  	it "should have a valid factory" do
-		  	comp = build(:job)
-		  	expect(comp.save).to eq true 
-		  end
-		  it { should have_one(:experience) }
-  		it { should have_one(:employmenttype) }
-  		it { should have_one(:jobfunction) }
-  		it { should have_one(:status) }  		  
-	end
-=======
-  describe "validation" do 
-  	it "should have a valid factory" do 
-  		j = build(:job)
-  		expect(j.save).to eq true
+  describe "validations" do 
+      it "should have a valid factory" do
+        comp = build(:job)
+        expect(comp.save).to eq true 
+      end
+      it { should have_one(:experience) }
+      it { should have_one(:employmenttype) }
+      it { should have_one(:jobfunction) }
+      it { should have_one(:status) }     
   end
-  it { should belong_to(:user) }
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:city) }
-  it { should validate_presence_of(:state) }
-  
->>>>>>> job-model
 end
