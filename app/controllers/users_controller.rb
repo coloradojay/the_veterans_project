f4d99853900058fdb1b008cce60f90f618e03d89
ponlok huot <phuot1980@gmail.com>
2015-02-15 22:18:22 -0800

    added user->education nested form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
 	def edit
 		@user = User.find(params[:id])
-		@submit_message = 'Edit Account'
+		@submit_message = 'Update Account'
 	end
 
 	def post
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
 
 		if @user.update(user_params)
 			flash[:notice] = "This user has been updated."
-			redirect_to users_path
+			redirect_to @user
 		else
 			render :edit
 		end
@@ -51,7 +51,9 @@ class UsersController < ApplicationController
 
 	private
 		def user_params
-			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :address, :work_history, :military_experience, :skill, :education)
+			params.require(:user).permit(:first_name, :last_name, 
+				:email, :password, :password_confirmation,
+				educations_attributes: [:id, :school_name, :yr_attended_from, :yr_attended_to, :degree_name, :field_of_study, :description, :_destroy])
 		end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ActiveRecord::Base
 	has_secure_password
-
 	has_one :military_experience
 	has_one :address
-
 	has_many :educations
 	has_many :work_histories
-	
 	has_and_belongs_to_many :skills
+
+	accepts_nested_attributes_for :educations, reject_if: :all_blank, allow_destroy: true
 
 	validates :first_name, presence: true
 	validates :last_name, presence: true
