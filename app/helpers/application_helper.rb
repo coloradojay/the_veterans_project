5fce27487183731e7e573dbc750d016357e9c38f
ponlok huot <phuot1980@gmail.com>
2015-02-17 22:22:04 -0800

    fix show for veteran and company

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
 			@current_user ||= User.find_by_id(session[:user_id])
 	end
 
+	def current_user_is_business?
+		current_user.business
+	end
+
+	def current_user_is_veteran?
+		!current_user.business
+	end
 end
