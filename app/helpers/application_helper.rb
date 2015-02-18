9ad46b9db6ea25a3e72400eafe254877c5b2d546
coloradojay <delarosa.j@gmail.com>
2015-02-17 23:03:48 -0800

    Job new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-
 	def current_user
-			@current_user ||= User.find_by_id(session[:user_id])
+		@current_user ||= User.find_by_id(session[:user_id])
 	end
 
 	def current_user_is_business?
