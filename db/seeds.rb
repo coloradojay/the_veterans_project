c2adb8bc9b26995d8e291d80268a3ef4d73be06c
coloradojay <delarosa.j@gmail.com>
2015-02-19 18:47:53 -0800

    Modifying seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,19 @@
 # EmploymentType.where(name: 'Full-Time').first_or_create!
 # EmploymentType.where(name: 'Contractor').first_or_create!
 
-Type.where(name: "Public").first_or_create!
-Type.where(name: "Private").first_or_create!
+Types.where(name: "Public").first_or_create!
+Types.where(name: "Private").first_or_create!
 
-Sector.where(name: "Accounting, tax preparation, book keeping and payroll services").first_or_create!
-Sector.where(name: "Legal services").first_or_create!
-Sector.where(name: "Oil and gas extraction").first_or_create!
-Sector.where(name: "Commercial and industrial machinery and equipment rental and leasing").first_or_create!
-Sector.where(name: "Real estate").first_or_create!
-Sector.where(name: "Medical").first_or_create!
-Sector.where(name: "Management companies").first_or_create!
-Sector.where(name: "Technology").first_or_create!
-Sector.where(name: "Banking").first_or_create!
+Sectors.where(name: "Accounting, tax preparation, book keeping and payroll services").first_or_create!
+Sectors.where(name: "Legal services").first_or_create!
+Sectors.where(name: "Oil and gas extraction").first_or_create!
+Sectors.where(name: "Commercial and industrial machinery and equipment rental and leasing").first_or_create!
+Sectors.where(name: "Real estate").first_or_create!
+Sectors.where(name: "Medical").first_or_create!
+Sectors.where(name: "Management companies").first_or_create!
+Sectors.where(name: "Technology").first_or_create!
+Sectors.where(name: "Banking").first_or_create!
+
 
 
 
