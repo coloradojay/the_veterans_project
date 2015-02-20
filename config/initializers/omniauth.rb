bf0da58c0bf9b9188b8fd12b6b80c1385c4ffef9
coloradojay <delarosa.j@gmail.com>
2015-02-19 18:00:11 -0800

    Updating user show with IDme logo and script

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :idme, ENV["IDME_CLIENT"], ENV["IDME_SECRET"],
+  provider :idme, ENV['IDME_CLIENT'], ENV['IDME_SECRET'],
   :scope => "military"
 end
